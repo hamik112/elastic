f1c6ce636a1b5252f253fdb6033765a4ae8bf3b4
Daniel Kang <me@daniel.gs>
2013-09-15 15:45:38 -0700

    try to stop services

--- nginx-uwsgi/nginx-uwsgi-django-bootstrap.sh
+++ nginx-uwsgi/nginx-uwsgi-django-bootstrap.sh
@@ -11,11 +11,13 @@ if [ `whoami` != "root" ] ; then
 	die "You must be root to run this."
 fi
 
-# install software
+# install required
 yum upgrade -y || die "Failed to update yum."
 yum install -y gcc-c++ libxml2-python libxml2-devel python-devel || die "Failed to install required software."
-yum install -y nginx-1.2.9 || die "Failed to install nginx."
-easy_install uwsgi==1.9.16 || die "Failed to install uWSGI."
+
+# try stopping services
+service nginx stop 2> /dev/null
+service uwsgi stop 2> /dev/null
 
 # install Django and an example Django app
 easy_install Django==1.5.4 || die "Failed to install Django."
@@ -24,11 +26,13 @@ mkdir -p /usr/local/src/django-helloworld
 django-admin.py startproject helloworld /usr/local/src/django-helloworld
 
 # config & start nginx 
+yum install -y nginx-1.2.9 || die "Failed to install nginx."
 curl -L https://raw.github.com/d5/elastic/master/nginx-uwsgi/nginx-uwsgi.conf > /etc/nginx/nginx.conf || die "Failed to update nginx config."
 chkconfig nginx on
 service nginx start || die "Failed to start nginx service."
 
 # config & start uWSGI
+easy_install uwsgi==1.9.16 || die "Failed to install uWSGI."
 curl -L https://raw.github.com/d5/elastic/master/nginx-uwsgi/nginx-uwsgi-initd > /etc/init.d/uwsgi || die "Failed to set up uWSGI service."
 chmod +x /etc/init.d/uwsgi
 chkconfig uwsgi on
