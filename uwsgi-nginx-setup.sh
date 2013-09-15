51a2f0c32a70db544b938680e4e49d51d5b10c09
Daniel Kang <me@daniel.gs>
2013-09-14 20:50:01 -0700

    Update uwsgi-nginx-setup.sh

--- uwsgi-nginx-setup.sh
+++ uwsgi-nginx-setup.sh
@@ -3,6 +3,7 @@ yum install -y gcc-c++ libxml2-python libxml2-devel python-devel
 easy_install uwsgi==1.9.16
 easy_install Flask==0.10.1
 yum install -y nginx-1.2.9
+curl -L https://raw.github.com/d5/elastic/master/uwsgi_params > /etc/nginx/uwsgi_params
 curl -L https://raw.github.com/d5/elastic/master/uwsgi-nginx.conf > /etc/nginx/nginx.conf
 service nginx start
 chkconfig nginx on
