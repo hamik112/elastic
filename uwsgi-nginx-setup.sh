8bae3fefbc2646209a529a88e1073ef0f6a58eda
Daniel Kang <me@daniel.gs>
2013-09-15 00:16:52 -0700

    Update uwsgi-nginx-setup.sh

--- uwsgi-nginx-setup.sh
+++ uwsgi-nginx-setup.sh
@@ -1,5 +1,5 @@
 yum upgrade -y
-yum install -y gcc-c++ libxml2-python libxml2-devel python-devel
+yum install -y gcc-c++ libxml2-python libxml2-devel python-devel git
 easy_install uwsgi==1.9.16
 easy_install Flask==0.10.1
 yum install -y nginx-1.2.9
