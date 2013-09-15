f7c19ac23e864c822ea5b3b470ee6e4ba1b11445
Daniel Kang <me@daniel.gs>
2013-09-15 00:08:38 -0700

    change install dir

--- redis-setup.sh
+++ redis-setup.sh
@@ -15,7 +15,7 @@ wget http://download.redis.io/releases/redis-2.6.16.tar.gz
 tar xzf redis-2.6.16.tar.gz
 rm -f redis-2.6.16.tar.gz
 cd redis-2.6.16
-make PREFIX=/usr/bin install
+make PREFIX=/usr install
 
 die () {
 	echo "ERROR: $1. Aborting!" 
