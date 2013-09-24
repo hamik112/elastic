38dbd320ad21b1cae5f76e2a4406ddace2b927f1
Daniel Kang <me@daniel.gs>
2013-09-24 01:35:16 -0700

    add beer scripts

---
+++ beer/README.md
@@ -0,0 +1,20 @@
+# Service Deploy
+
+# Bootstrapping
+
+To bootstrap the front-end nodes, run:
+
+```
+curl -L https://raw.github.com/d5/elastic/master/nginx-uwsgi/nginx-uwsgi-flask-bootstrap.sh | sudo sh
+curl -L https://raw.github.com/d5/elastic/master/beer/bootstrap.sh | sudo sh
+```
+
+To bootstrap the back-end nodes, run:
+
+```
+curl -L https://raw.github.com/d5/elastic/master/redis/redis-bootstrap.sh | sudo sh
+```
+
+## Tested EC2 images
+
+* Amazon Linux AMI x86_64 PV EBS - amzn-ami-pv-2013.03.1.x86_64-ebs (ami-0358ce33)
---
+++ beer/bootstrap.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+die() { echo "ERROR: $1. Aborting!"; exit 1; }
+
+# install dependencies
+easy_install Flask-OpenID==1.1.1 || die "Failed to install Flask-OpenID."
+
+# stop services
+service nginx stop || die "Failed to stop nginx service."
+service uwsgi stop || die "Failed to stop uWSGI service."
+
+# install the lastest code
+rm -rf /usr/local/src/beer
+git clone https://d5dev@bitbucket.org/d5dev/beer.git /usr/local/src/beer || die "Failed to get the code."
+
+# configure
+cp -f /usr/local/src/beer/deploy/uwsgi.yaml /etc/uwsgi.yaml
+
+# start services
+service uwsgi start || die "Failed to start uWSGI service."
+service nginx start || die "Failed to start nginx service."
+
+
+
+
