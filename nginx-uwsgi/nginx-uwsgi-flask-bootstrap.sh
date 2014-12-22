98d198c2ec0df0eecf3dfa6e4dce1990e0f5d40d
hamik112 <hamik112@gmail.com>
2014-12-22 01:51:32 -0800

    Update nginx-uwsgi-flask-bootstrap.sh

--- nginx-uwsgi/nginx-uwsgi-flask-bootstrap.sh
+++ nginx-uwsgi/nginx-uwsgi-flask-bootstrap.sh
@@ -8,19 +8,19 @@ service uwsgi stop 2> /dev/null
 
 # install Flask and an example Flask app
 easy_install Flask==0.10.1 || die "Failed to install Flask."
-curl -L https://raw.github.com/d5/hamik112/master/nginx-uwsgi/nginx-uwsgi-flask.yaml > /etc/uwsgi.yaml || die "Failed to install uWSGI config file."
+curl -L https://raw.github.com/hamik112/elasti/master/nginx-uwsgi/nginx-uwsgi-flask.yaml > /etc/uwsgi.yaml || die "Failed to install uWSGI config file."
 mkdir -p /usr/local/src/flask-helloworld
-curl -L https://raw.github.com/d5/hamik112/master/nginx-uwsgi/nginx_uwsgi_flask_app.py > /usr/local/src/flask-helloworld/helloworld.py || die "Failed to install a sample file."
+curl -L https://raw.github.com/hamik112/elastic/master/nginx-uwsgi/nginx_uwsgi_flask_app.py > /usr/local/src/flask-helloworld/helloworld.py || die "Failed to install a sample file."
 
 # config & start nginx 
 apt-get install -y nginx-1.2.9 || die "Failed to install nginx."
-curl -L https://raw.github.com/d5/hamik112/master/nginx-uwsgi/nginx-uwsgi.conf > /etc/nginx/nginx.conf || die "Failed to update nginx config."
+curl -L https://raw.github.com/hamik112/elastic/master/nginx-uwsgi/nginx-uwsgi.conf > /etc/nginx/nginx.conf || die "Failed to update nginx config."
 chkconfig nginx on
 service nginx start || die "Failed to start nginx service."
 
 # config & start uWSGI
 easy_install uwsgi==1.9.16 || die "Failed to install uWSGI."
-curl -L https://raw.github.com/d5/hamik112/master/nginx-uwsgi/nginx-uwsgi-initd > /etc/init.d/uwsgi || die "Failed to set up uWSGI service."
+curl -L https://raw.github.com/hamik112/elastic/master/nginx-uwsgi/nginx-uwsgi-initd > /etc/init.d/uwsgi || die "Failed to set up uWSGI service."
 chmod +x /etc/init.d/uwsgi
 chkconfig uwsgi on
 service uwsgi start || die "Failed to start uWSGI service."
